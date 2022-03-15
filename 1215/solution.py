81d0efc1cd7aecdce0de0a2c2ce8afe371877b03
seyoung <seftware99@gmail.com>
2022-03-15 09:18:25 +0900

    fix: readability

--- 1215/solution.py
+++ 1215/solution.py
@@ -1,4 +1,4 @@
-def checkp(s, p_len):
+def check_palin(s, p_len):
     for i in range(p_len//2):
         if s[i] != s[-1*(1+i)]:
             return 0
@@ -8,24 +8,23 @@ T = 10
 BOARD_SIZE = 8
 for i in range(T):
     answer = 0
-    p_len = int(input())
     arr = []
-    cnt = 0
+    p_len = int(input())
     for j in range(BOARD_SIZE):
         tmp = list(input())
         arr.append(tmp)
     # 가로
     for row in range(BOARD_SIZE):
         for col in range(BOARD_SIZE-p_len+1):
-            str1 = ""
+            substr = ""
             for j in range(p_len):
-                str1 += arr[row][col+j]
-            answer += checkp(str1, p_len)
+                substr += arr[row][col+j]
+            answer += check_palin(substr, p_len)
     # 세로
     for row in range(BOARD_SIZE-p_len+1):
         for col in range(BOARD_SIZE):
-            str2 = ""
+            substr = ""
             for j in range(p_len):
-                str2 += arr[row+j][col]
-            answer += checkp(str2, p_len)
-    print(f"#{i+1} {answer}")
+                substr += arr[row+j][col]
+            answer += check_palin(substr, p_len)
+    print(f"#{i} {answer}")
