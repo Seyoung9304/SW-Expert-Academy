67ec5f8cc47cddcfb63f28bd0b0436b2f3d79811
seyoung <seftware99@gmail.com>
2022-03-23 17:07:53 +0900

    fix: improve execution time

--- 1267/solution.py
+++ 1267/solution.py
@@ -1,5 +1,3 @@
-from collections import deque
-# Topology sort
 TC = 10
 for tc in range(1, TC+1):
     V, E = map(int, input().split())
@@ -7,25 +5,28 @@ for tc in range(1, TC+1):
     indegree = [0] * (V+1)
     child_parent = [[] for _ in range(V+1)]
     for i in range(0, 2*E, 2):
-        parent = edges[i]
-        child = edges[i+1]
+        parent, child = edges[i], edges[i+1]
         indegree[child] += 1
         child_parent[child].append(parent)
 
     # find start node
-    todo = deque()   # work queue
+    todo = []   # work queue
     for i in range(1, V+1):
         if len(child_parent[i])==0:
             todo.append(i)
     
-    print(f"#{tc}", end=" ")
     # topology sort
-    while todo:
-        p = todo.popleft()
-        print(p, end=" ") # done
+    front = 0
+    while len(todo)<V:
+        p = todo[front]
         for c in range(1, V+1):
             if p in child_parent[c]:
                 indegree[c] -= 1
                 if indegree[c]==0:
                     todo.append(c)
+        front += 1
+
+    print(f"#{tc}", end=" ")
+    for i in todo:
+        print(i, end=" ")
     print("")
